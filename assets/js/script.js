4d54cc3cc5cbadbdc8fc741a40aba38eeb372af6
Tim Schulz <tim@getbej.com>
2022-01-12 12:29:49 +0000

    Add constants for DOM manipulation with JS

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Declare constants for DOM manupulation and to grab input values
+ */
+
+const retirementAge = document.getElementById("retirement-age");
+const income = document.getElementById("income-el");
+const cost = document.getElementById("cost-el");
+const netWorth = document.getElementById("net-worth-el");
+const interest = document.getElementById("interest-el");
+const age = document.getElementById("age-el");
+const button = document.getElementById("calculate");
+const portfolioValue = document.getElementById("portfolio-val");
+const savingsValue = document.getElementById("savings-val");
+const roiValue = document.getElementById("roi-val")
